8b70297f677b735d179ea76dc5a1be6f18267dc2
jykim <wlsdud4810@naver.com>
2024-07-11 14:30:25 +0900

    초기 커밋.

---
+++ Git Base/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Base
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hi");
+
+			// 키 기다리기
+			Console.WriteLine("아무 키나 누르세요...");
+			Console.ReadKey();
+		}
+	}
+}
